aa5fca3cea289abc2206783fb564f8ef90610639
Patrick <patricksteveharrison@gmail.com>
2025-09-16 18:33:06 +0800

    Implement angle_pid and fixed speed_pid functions

--- Core/Tasks/Src/motor_control.c
+++ Core/Tasks/Src/motor_control.c
@@ -53,6 +53,7 @@ void yangle_pid(double setpoint, double curr_pt, motor_data_t *motor, float imu_
 			((imu_ang_diff < -PI) ? imu_ang_diff + (2*PI) : imu_ang_diff);
 	float imu_rpm = (imu_ang_diff  * time_mult)/(2 * PI);
 	*prev_imu_data = imu_data;
+
 	motor->angle_pid.integral += motor->angle_pid.error[0]  * motor->angle_pid.ki;
 	float_minmax(&motor->angle_pid.integral, motor->angle_pid.int_max, 0);
 	float rpm_iOut = motor->angle_pid.ki;
@@ -75,6 +76,29 @@ void yangle_pid(double setpoint, double curr_pt, motor_data_t *motor, float imu_
  */
 void angle_pid(double setpoint, double curr_pt, motor_data_t *motor) {
 	// todo: 2. Implement pid
+	float ang_diff = (setpoint - curr_pt);
+
+	motor->angle_pid.last_time[1] = motor->angle_pid.last_time[0];
+	motor->angle_pid.last_time[0] = get_microseconds();
+
+	if(motor->angle_pid.last_time[0] <= motor->angle_pid.last_time[1]){
+		return;
+	}
+
+	motor->angle_pid.error[1] = motor->angle_pid.error[0];
+	motor->angle_pid.error[0] = ang_diff;
+
+	float p_out = motor->angle_pid.kp * ang_diff;
+	float d_out = motor->angle_pid.kd * (motor->angle_pid.error[0] - motor->angle_pid.error[1]);
+
+	motor->angle_pid.integral += motor->angle_pid.error[0] * motor->angle_pid.ki;
+	float_minmax(&motor->angle_pid.integral, motor->angle_pid.int_max, -motor->angle_pid.int_max);
+	float i_out = motor->angle_pid.integral;
+
+	motor->angle_pid.output = p_out + d_out + i_out;
+	float_minmax(&motor->angle_pid.output, motor->angle_pid.max_out, -motor->angle_pid.max_out);
+
+	speed_pid(motor->angle_pid.output, motor->raw_data.rpm, &motor->rpm_pid);	
 }
 
 
@@ -95,19 +119,20 @@ void speed_pid(double setpoint, double curr_pt, pid_data_t *pid) {
 	pid->last_time[1] = pid->last_time[0];
 	pid->last_time[0] = get_microseconds();
 
-	uint32_t time_diff = (pid->last_time[0] - pid->last_time[1])*10e-6;
+	uint32_t time_diff = (pid->last_time[0] - pid->last_time[1])*1e-6;
 
 	pid->error[1] = pid->error[0];
 	pid->error[0] = diff;
 
 	pid->integral += diff*time_diff;
-	float_minmax(pid->integral, pid->int_max, 0);
+	float_minmax(&pid->integral, pid->int_max, -pid->int_max);
 	
 	float rpm_pout = pid->kp * diff;
 	float rpm_iout = pid->integral*pid->ki;
 	float rpm_dout = pid->kd * (pid->error[0]-pid->error[1]);
 
 	float final = rpm_pout + rpm_iout + rpm_dout;
+	float_minmax(&final, pid->max_out, -pid->max_out);
 	pid->output = final;
 
 }
